b4eede2414e64453a04fff1aa8a9ffb6333997b4
Calvin Goodrich <cgoodric@yahoo.com>
2014-11-20 10:34:28 -0800

    Updates to licensing notices

--- JavaSource/TextUtils/src/com/cisco/dvbu/ps/utils/text/CSVFromCISQuery.java
+++ JavaSource/TextUtils/src/com/cisco/dvbu/ps/utils/text/CSVFromCISQuery.java
@@ -44,6 +44,20 @@ package com.cisco.dvbu.ps.utils.text;
 	             qualified (and the existing qualifiers escaped.)
 	
 	(c) 2010, 2014 Cisco and/or its affiliates. All rights reserved.
+
+    This software is released under the Eclipse Public License. The details can be found in the file LICENSE. 
+    Any dependent libraries supplied by third parties are provided under their own open source licenses as 
+    described in their own LICENSE files, generally named .LICENSE.txt. The libraries supplied by Cisco as 
+    part of the Composite Information Server/Cisco Data Virtualization Server, particularly csadmin-XXXX.jar, 
+    csarchive-XXXX.jar, csbase-XXXX.jar, csclient-XXXX.jar, cscommon-XXXX.jar, csext-XXXX.jar, csjdbc-XXXX.jar, 
+    csserverutil-XXXX.jar, csserver-XXXX.jar, cswebapi-XXXX.jar, and customproc-XXXX.jar (where -XXXX is an 
+    optional version number) are provided as a convenience, but are covered under the licensing for the 
+    Composite Information Server/Cisco Data Virtualization Server. They cannot be used in any way except 
+    through a valid license for that product.
+
+    This software is released AS-IS!. Support for this software is not covered by standard maintenance agreements with Cisco. 
+    Any support for this software by Cisco would be covered by paid consulting agreements, and would be billable work.
+
  */
 
 import com.compositesw.common.logging.Logger;
